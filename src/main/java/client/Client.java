1517de18102c25e966895f2114ff451c1cb2cf58
voldemorten <mortendlaursen@gmail.com>
2019-10-08 07:47:28 +0200

    fixed solution

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -6,6 +6,7 @@ import server.models.User;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -13,7 +14,7 @@ public class Client {
     //configure your client here
     private static final String BASE = "http://localhost:8080/api/";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         clearConsole();
         System.out.println("Main menu:");
         System.out.println("1. Get all users");
@@ -31,12 +32,47 @@ public class Client {
                     System.out.println("\nGetting all users");
                     getAllUsers();
                     System.out.println("\n returning to main menu \n");
-
                     main(null);
                 case 2:
-                    break;
+                    clearConsole();
+                    System.out.println("Enter user id");
+                    try {
+                        int userid = Integer.parseInt(br.readLine());
+                        User u = getUser(userid);
+                        if(u == null) System.out.println("No user by that id");
+                        else System.out.println(u);
+                        returnToMainMenu();
+                    } catch (Exception e) {
+                        System.out.println("invalid input\nReturning to main menu");
+                        try {
+                            returnToMainMenu();
+                        } catch (Exception ee) {
+                            System.out.println(ee.getMessage());
+                        }
+                    }
+
                 case 3:
-                    break;
+                    clearConsole();
+                    System.out.println("Enter user id");
+                    try {
+                        int userId = Integer.parseInt(br.readLine());
+                        System.out.println("Enter user name");
+                        String userName = br.readLine();
+                        System.out.println("Enter user age");
+                        int userAge = Integer.parseInt(br.readLine());
+                        User u = new User(userId, userName, userAge);
+                        addUser(u);
+                        returnToMainMenu();
+
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.out.println("invalid input\nReturning to main menu");
+                        try {
+                            returnToMainMenu();
+                        } catch(Exception eee) {
+                            System.out.println(eee.getMessage());
+                        }
+                    }
                 case 4:
                     System.exit(1);
                     break;
@@ -51,8 +87,40 @@ public class Client {
         }
     }
 
-    public static void getAllUsers() throws Exception{
+    public static void getAllUsers() {
         String url = BASE+"users";
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+
+            User[] users = new Gson().fromJson(response.toString(), User[].class);
+            for(User u : users) {
+                System.out.println(u);
+            }
+
+            returnToMainMenu();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static User getUser(int id) throws Exception {
+        String url = BASE+"users/"+id;
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
@@ -70,12 +138,55 @@ public class Client {
 
         in.close();
 
-        User[] users = new Gson().fromJson(response.toString(), User[].class);
-        for(User u : users) {
-            System.out.println(u);
+        User u = new Gson().fromJson(response.toString(), User.class);
+        return u;
+    }
+
+    public static void addUser(User u) {
+        //String url = BASE+"users";
+        String url = "http://localhost:8080/api/users/";
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestMethod("POST");
+
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+
+            String jsonInputString = new Gson().toJson(u);
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes();
+                os.write(input, 0, input.length);
+            } catch(Exception e) {
+                System.out.println("Something went wrong");
+                try {
+                    returnToMainMenu();
+                } catch (Exception ee) {
+                    System.out.println(ee.getMessage());
+                }
+            }
+
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+                br.close();
+                System.out.println("User created");
+            } catch (Exception e3) {
+                System.out.println(e3.getMessage());
+            }
+        } catch (Exception eee) {
+            System.out.println(eee.getMessage());
         }
+    }
 
-
+    public static void returnToMainMenu() throws Exception {
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(System.in)
         );
